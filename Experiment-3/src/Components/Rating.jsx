fca01615e0538bce4a7439ffa8aa672d65090927
Karan-Sajwan <sajwankaran04gmail.com>
2026-01-20 10:00:15 +0530

    Final

--- Experiment-3/src/Components/Rating.jsx
+++ Experiment-3/src/Components/Rating.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+
 export default function Rating() {
-  return <div className="rating">⭐⭐⭐⭐☆</div>;
+  const [rating, setRating] = useState(0);
+
+  return (
+    <div>
+      <p>Rate this:</p>
+      {[1, 2, 3, 4, 5].map((star) => (
+        <span
+          key={star}
+          onClick={() => setRating(star)}
+          style={{
+            cursor: "pointer",
+            fontSize: "24px",
+            color: star <= rating ? "#facc15" : "#64748b",
+          }}
+        >
+          ★
+        </span>
+      ))}
+      <p>Your Rating: {rating}</p>
+    </div>
+  );
 }
