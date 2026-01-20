93a774953d8de9b170c6d9b3f6c09870c1780a5a
Karan-Sajwan <sajwankaran04gmail.com>
2026-01-20 10:11:26 +0530

    Final

--- Experiment-3/src/App.jsx
+++ Experiment-3/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
-import "./App.css";
 
 function App() {
   return (
